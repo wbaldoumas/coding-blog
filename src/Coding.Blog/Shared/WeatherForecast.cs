80f7d0f3886317e64d44407c5adc62785bfc852c
William Baldoumas <itabaiyu@gmail.com>
2021-08-30 19:02:09 -0400

    Project Setup

---
+++ src/Coding.Blog/Shared/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Coding.Blog.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; init; }
+
+        public int TemperatureC { get; init; }
+
+        public string Summary { get; init; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
