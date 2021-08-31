f9bd6ead538073672e939ff6bb510473426618de
William Baldoumas <itabaiyu@gmail.com>
2021-08-30 21:18:27 -0400

    Kubernetes Setup

--- src/Coding.Blog/Server/Program.cs
+++ src/Coding.Blog/Server/Program.cs
@@ -5,10 +5,7 @@ namespace Coding.Blog.Server
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
