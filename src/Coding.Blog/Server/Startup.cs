de612eeba747d2ece481164087bedcb3e54a3c13
Itabaiyu <45316999+itabaiyu@users.noreply.github.com>
2021-08-30 21:19:44 -0400

    Kubernetes Setup (#3)

--- src/Coding.Blog/Server/Startup.cs
+++ src/Coding.Blog/Server/Startup.cs
@@ -16,7 +16,7 @@ namespace Coding.Blog.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHealthChecks();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -24,6 +24,8 @@ namespace Coding.Blog.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHealthChecks("/healthz");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -32,7 +34,6 @@ namespace Coding.Blog.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
