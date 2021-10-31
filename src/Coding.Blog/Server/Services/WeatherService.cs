6f47cb3ec7068d3d40ef1d8f796565126535d7be
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2021-10-31 18:38:59 -0400

    GRPC Experiments (#19)

---
+++ src/Coding.Blog/Server/Services/WeatherService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Coding.Blog.Shared;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace Coding.Blog.Server.Services
+{
+    public class WeatherService : WeatherForecasts.WeatherForecastsBase
+    {
+        private static readonly string[] Summaries = {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public override Task<WeatherReply> GetWeather(WeatherForecast request, ServerCallContext context)
+        {
+            var reply = new WeatherReply();
+            var rng = new Random();
+
+            reply.Forecasts.Add(Enumerable.Range(1, 10).Select(_ => new WeatherForecast
+            {
+                Date = Timestamp.FromDateTime(DateTime.UtcNow),
+                TemperatureC = rng.Next(20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }));
+
+            return Task.FromResult(reply);
+        }
+    }
+}
