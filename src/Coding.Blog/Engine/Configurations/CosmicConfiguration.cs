70ec97e7b1d67d8a4efecd71ae7fb1376c17f6a1
William Baldoumas <william.baldoumas@gmail.com>
2021-11-12 19:28:42 -0500

    Add Secrets

---
+++ src/Coding.Blog/Engine/Configurations/CosmicConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Coding.Blog.Engine.Configurations;
+
+public class CosmicConfiguration
+{
+    public static string Key => "Cosmic";
+
+    public string BucketSlug { get; set; } = string.Empty;
+
+    public string ReadKey { get; set; } = string.Empty;
+}
