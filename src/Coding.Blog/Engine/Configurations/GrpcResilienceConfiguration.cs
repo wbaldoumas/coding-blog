9897798c8e0ef73a48f11b2afd7aeb62139cfe91
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2021-11-27 17:00:47 -0500

    GRPC Enhancements (#25)

---
+++ src/Coding.Blog/Engine/Configurations/GrpcResilienceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Coding.Blog.Engine.Configurations;
+
+[ExcludeFromCodeCoverage]
+public record GrpcResilienceConfiguration : IKeyedConfiguration
+{
+    public string Key => "GrpcResilience";
+
+    public int MaxAttempts { get; set; }
+
+    public int InitialBackoffMilliseconds { get; set; }
+
+    public int MaxBackoffMilliseconds { get; set; }
+
+    public double BackoffMultiplier { get; set; }
+}
