eef625d54060f2303ad6f6f8f746a593032d81e5
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2022-02-13 20:19:30 -0500

    Implement Read Time Estimation (#89)

---
+++ src/Coding.Blog/Engine/Utilities/ReadTimeEstimator.cs
@@ -0,0 +1,22 @@
+﻿namespace Coding.Blog.Engine.Utilities;
+
+internal class ReadTimeEstimator : IReadTimeEstimator
+{
+    private const double AverageWordsPerMinute = 250.00;
+
+    private readonly IStringSanitizer _sanitizer;
+
+    public ReadTimeEstimator(IStringSanitizer sanitizer) => _sanitizer = sanitizer;
+
+    public TimeSpan Estimate(string content)
+    {
+        var sanitizedString = _sanitizer.Sanitize(content);
+        var wordCount = sanitizedString.Split(' ').Length;
+
+        var estimatedReadingMinutes = wordCount / AverageWordsPerMinute;
+
+        return estimatedReadingMinutes < 1
+            ? TimeSpan.FromMinutes(1)
+            : TimeSpan.FromMinutes(Math.Round(estimatedReadingMinutes, MidpointRounding.AwayFromZero));
+    }
+}
---
+++ src/Tests/Coding.Blog.UnitTests/Utilities/StringSanitizerTests.cs
@@ -0,0 +1,45 @@
+﻿using Coding.Blog.Engine.Utilities;
+using FluentAssertions;
+using Markdig;
+using NUnit.Framework;
+
+namespace Coding.Blog.UnitTests.Utilities;
+
+[TestFixture]
+public class StringSanitizerTests
+{
+    [Test]
+    public void StringSanitizer_generates_expected_string()
+    {
+        // arrange
+        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+        var sanitizer = new StringSanitizer(pipeline);
+
+        // act
+        var sanitizedString = sanitizer.Sanitize(MarkdownString);
+
+        // assert
+        sanitizedString.Should().Be(SanitizedString);
+    }
+
+    private const string MarkdownString = @"
+# This is a header
+
+Here is some other content with a [link](https://google.com).
+
+## Code example sub-header
+
+```
+var foo = new Bar();
+var bar = foo.Bar();
+```
+";
+
+    private const string SanitizedString = @"This is a header
+Here is some other content with a link.
+Code example sub-header
+var foo = new Bar();
+var bar = foo.Bar();
+";
+
+}
