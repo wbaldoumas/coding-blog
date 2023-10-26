9cd8f13a2e96d1f7e00843e4506c8289843adee0
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2023-10-26 14:34:36 -0700

    Auto-warm Caches (#405)

---
+++ src/Tests/Coding.Blog.UnitTests/Jobs/PostsWarmingJobTests.cs
@@ -0,0 +1,41 @@
+﻿using Coding.Blog.Engine.Clients;
+using Coding.Blog.Engine.Jobs;
+using Coding.Blog.Engine.Records;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Coding.Blog.UnitTests.Jobs;
+
+[TestFixture]
+internal sealed class PostsWarmingJobTests
+{
+    private ICosmicClient<CosmicPosts> _mockPostsClient = default!;
+
+    private ILogger<PostsWarmingJob> _mockLogger = default!;
+
+    private PostsWarmingJob _postsWarmingJob = default!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockPostsClient = Substitute.For<ICosmicClient<CosmicPosts>>();
+        _mockLogger = Substitute.For<ILogger<PostsWarmingJob>>();
+
+        _postsWarmingJob = new PostsWarmingJob(_mockPostsClient, _mockLogger);
+    }
+
+    [Test]
+    public async Task PostsWarmingJob_calls_posts_client()
+    {
+        // act
+        await _postsWarmingJob.Execute(default!).ConfigureAwait(false);
+
+        // assert
+        _mockLogger.Received(1).LogInformation("Warming posts cache...");
+
+        await _mockPostsClient.Received(1).GetAsync().ConfigureAwait(false);
+
+        _mockLogger.Received(1).LogInformation("Finished warming posts cache.");
+    }
+}
