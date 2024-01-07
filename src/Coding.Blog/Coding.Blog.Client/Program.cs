684c44565cfaaf44bdaa8573bd9ae9a0a3e9e014
Will Baldoumas <william.baldoumas@gmail.com>
2024-01-06 19:20:25 -0800

    Post Change Fix

--- src/Coding.Blog/Coding.Blog.Client/Program.cs
+++ src/Coding.Blog/Coding.Blog.Client/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Services.ConfigureServices(builder.Configuration);
 
 await builder.Build()
