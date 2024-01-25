e5280f35d7eae214ee5d94fd9c63a0f01276130d
Will Baldoumas <william.baldoumas@gmail.com>
2024-01-24 20:45:04 -0800

    Better Giscus Handling while Globally InteractiveAuto

--- src/Coding.Blog/Coding.Blog.Client/Services/JSInteropService.cs
+++ src/Coding.Blog/Coding.Blog.Client/Services/JSInteropService.cs
@@ -5,4 +5,10 @@ namespace Coding.Blog.Client.Services;
 public sealed class JSInteropService(IJSRuntime jsRuntime) : IJSInteropService
 {
     public ValueTask ResetScrollPositionAsync() => jsRuntime.InvokeVoidAsync("resetScrollPosition");
+
+    public async ValueTask LoadGiscusAsync()
+    {
+        await jsRuntime.InvokeVoidAsync("removeGiscusFrame").ConfigureAwait(false);
+        await jsRuntime.InvokeVoidAsync("addGiscusFrame").ConfigureAwait(false);
+    }
 }
